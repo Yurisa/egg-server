3d63debde885cde10ffaa7ec7eb75b5e3e5845ff
Yurisa <779509411@qq.com>
2019-04-28 12:00:49 +0800

    feature:获取所有上传文件信息

--- app/service/home.js
+++ app/service/home.js
@@ -2,7 +2,6 @@ const Service = require('egg').Service;
 
 class HomeService extends Service {
   async findAll() {
-    console.log(this.app.mysql)
     const data = await this.app.mysql.select('upload_file');
     return data;
   }
